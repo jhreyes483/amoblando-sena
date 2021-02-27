4330525b81b4cf0ee89be86bb03ddc630cdbc864
Javier <jhreyes483@misena.edu.co>
2021-02-27 14:53:48 -0500

    Shore

--- application/Config.php
+++ application/Config.php
@@ -8,7 +8,7 @@ define("imgProducto", "ProductoSinImagen.png");
 
 
 //controlador por defecto de nuestra aplicacion
-define('BASE_URL', 'https://localhost/amoblando01/');  
+define('BASE_URL', 'https://dashboard.heroku.com/amoblando-mvc/');  
 define('DEFAULT_CONTROLLER', 'index');
 define('DEFAULT_LAYOUT', 'layout1');
 
